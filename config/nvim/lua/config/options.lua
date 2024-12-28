f080bbfdbea7296f7baed4e88ebeb166df99d13a
Ali Hussain Abid <ali.hussain.abid.246@outlook.com>
2024-12-29 00:51:27 +0500

    Neovim Config

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,13 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+local o = vim.o
+
+o.wrap = true
+o.tabstop = 8
+o.shiftwidth = 8
+o.softtabstop = 8
+o.expandtab = false
+o.autoindent = true
+o.relativenumber = true
+o.winfixwidth = true
