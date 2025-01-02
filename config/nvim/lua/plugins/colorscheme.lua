adc8a05c5ff75d85b5023d1b8891ad93e54558a6
Ali Hussain Abid <ali.hussain.abid.246@outlook.com>
2025-01-03 02:57:35 +0500

    Adjusted Theme

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,3 +1,4 @@
+-- TokyoNight colorscheme
 -- return {
 -- 	"folke/tokyonight.nvim",
 -- 	lazy = true,
@@ -12,20 +13,21 @@
 -- 	end,
 -- }
 
+-- Rose Pine colorscheme
 return {
 	{
 		"rose-pine/neovim",
 		priority = 1000,
 		config = function()
 			require("rose-pine").setup({
-				variant = "moon", -- auto, main, moon, or dawn
-				dark_variant = "moon", -- main, moon, or dawn
+				variant = "auto", -- auto, main, moon, or dawn
+				dark_variant = "main", -- main, moon, or dawn
 				dim_inactive_windows = false,
-				extend_background_behind_borders = true,
+				extend_background_behind_borders = false,
 
 				enable = {
 					terminal = true,
-					legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+					legacy_highlights = false, -- Improve compatibility for previous versions of Neovim
 					migrations = true, -- Handle deprecated options automatically
 				},
 
